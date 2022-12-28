3ba086cda96b256cc0f928fbb53507693010ed73
Julian Maldonado <julianmaldonadoag@gmail.com>
2022-12-28 10:17:33 -0500

    Added signup view

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,5 +19,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
-def hello_world(request):
-    return HttpResponse('<h1>Hola mundo</h1>')
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
+        })
+    else:
+        username = request.POST['username']
+        pwd1 = request.POST['password1']
+        pwd2 = request.POST['password2']
+
+        if pwd1 == pwd2:
+            try:
+                user = User.objects.create_user(username=username, password=pwd1)
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    'error': 'User already exists'
+                })
+        else:
+            return render(request, 'signup.html', {
+                'form': UserCreationForm(),
+                'error': 'Password do not match'
+            })
+        
+def tasks(request):
+    return render(request, 'tasks.html')
