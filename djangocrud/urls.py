ff99972448abc29c8e7f0266aa5c401154bf53fb
Julian Maldonado <julianmaldonadoag@gmail.com>
2022-12-29 14:53:17 -0500

    feat: update, complete and delete task

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('tasks/', views.tasks, name='tasks'),
     path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:task_id>/complete/', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete/', views.delete_task, name='delete_task'),
     path('tasks/create/', views.create_task, name='create_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
@@ -65,8 +66,23 @@ def tasks(request):
     return render(request, 'tasks.html', { 'tasks': tasks })
 
 def task_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, 'task_detail.html', { 'task': task })
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', { 'task': task, 'form': form })
+    # Update task.
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', { 
+                'task': task, 
+                'form': form,
+                'error': 'Error updating task'
+            })
 
 def create_task(request):
     if request.method == 'GET':
@@ -85,3 +101,16 @@ def create_task(request):
                 'form': TaskForm(),
                 'error': 'Please provide valid data'
             })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
